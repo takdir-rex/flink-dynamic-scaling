a6b9aaeefcbc27b14540d645381a01afe3353dca
Takdir <takdir.rex@gmail.com>
2024-07-29 09:54:27 +0900

    Update baseline-scaleout.sh

--- scripts/baseline-scaleout.sh
+++ scripts/baseline-scaleout.sh
@@ -8,19 +8,19 @@ do
       sed -i -e "s/p4${q}ss0$((i-1))/p4${q}ss0$i/g" "/home/flink1/docker/data/shared/nexmark/queries/${q}.sql"
       if [ "$q" = "q2" ]
       then
-         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q2 "8:2"
+         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q2 "4:2"
       fi
       if [ "$q" = "q3" ]
       then
-         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q3 "8:3"
+         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q3 "4:3"
       fi
       if [ "$q" = "q5" ]
       then
-         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q5 "8:7"
+         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q5 "4:7"
       fi
       if [ "$q" = "q8" ]
       then
-         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q8 "8:16"
+         docker exec jm /data/shared/nexmark/bin/run_query.sh oa q8 "4:16"
       fi
       /home/flink1/autoscale-experiment/autoscaler/rescale-scaleout.sh
       sleep 60s
